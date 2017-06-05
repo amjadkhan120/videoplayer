a09aab52325f364a40f2d74fe02a07cc2b0f22fd
amjadkhan120 <amjadkhan.kh@gmail.com>
2017-06-05 10:31:00 +0500

    video player

---
+++ app/index.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+import VideoPlayer from 'react-native-video-player';
+
+const VIMEO_ID = '179859217';
+
+export default class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      video: { width: undefined, height: undefined, duration: undefined },
+      thumbnailUrl: undefined,
+      videoUrl: undefined,
+    };
+  }
+
+  componentDidMount() {
+    global.fetch(`https://player.vimeo.com/video/${VIMEO_ID}/config`)
+      .then(res => res.json())
+      .then(res => this.setState({
+        thumbnailUrl: res.video.thumbs['640'],
+        videoUrl: res.request.files.hls.cdns[res.request.files.hls.default_cdn].url,
+        video: res.video,
+      }));
+  }
+
+  render() {
+    return (
+      <View>
+        <Text style={{ fontSize: 22, marginTop: 22 }}>React Native Video Player</Text>
+        <VideoPlayer
+          endWithThumbnail
+          thumbnail={{ uri: this.state.thumbnailUrl }}
+          video={{ uri: this.state.videoUrl }}
+          videoWidth={this.state.video.width}
+          videoHeight={this.state.video.height}
+          duration={this.state.video.duration/* I'm using a hls stream here, react-native-video
+            can't figure out the length, so I pass it here from the vimeo config */}
+        />
+      </View>
+    );
+  }
+}
